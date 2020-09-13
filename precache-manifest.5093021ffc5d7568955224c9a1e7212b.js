070aba79a093bd6eae7b1bf5af80a62df870ee44
Raja Singh <rajasingh9721@gmail.com>
2020-09-13 19:57:00 +0530

    Updates

---
+++ precache-manifest.5093021ffc5d7568955224c9a1e7212b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9032cca293c1f38d476136b849752f9d",
+    "url": "/thunk-app/index.html"
+  },
+  {
+    "revision": "b834a3ae37be4da723cb",
+    "url": "/thunk-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fbfede10284140d9b398",
+    "url": "/thunk-app/static/js/2.7cdd7f7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/thunk-app/static/js/2.7cdd7f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b834a3ae37be4da723cb",
+    "url": "/thunk-app/static/js/main.5f90db52.chunk.js"
+  },
+  {
+    "revision": "2c548fdc237af48f44ec",
+    "url": "/thunk-app/static/js/runtime-main.3aa8d700.js"
+  }
+]);
